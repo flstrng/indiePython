0df2e030acd9c9e1356a9a50c03dd3674abbfd74
flstrng <er8834567@gmail.com>
2023-03-06 18:49:29 +0300

    Solve 02_02_H

---
+++ 02_strings_lists/02_02_slices/02_02_H.py
@@ -0,0 +1,14 @@
+"""
+02_02_H
+Программа получает на вход строку.
+Выведите каждый третий символ строки в обратном порядке, начиная с последнего.
+
+Sample Input:
+The Big Bang Theory
+Sample Output:
+ye ag T
+"""
+
+phrase = input()
+print(phrase[::-3])
+
