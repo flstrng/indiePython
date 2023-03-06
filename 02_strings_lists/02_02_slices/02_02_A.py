8bd6714f4952382acd886a3bcf0df3c7a624ceba
flstrng <er8834567@gmail.com>
2023-03-06 16:39:33 +0300

    Solve 02_02_A

---
+++ 02_strings_lists/02_02_slices/02_02_A.py
@@ -0,0 +1,11 @@
+"""
+02_02_A
+Программа получает на вход строку и ваша задача вывести первый элемент данной строки
+
+Sample Input 1:
+Zemfira
+Sample Output 1:
+Z
+"""
+phrase = input()
+print(phrase[0])
