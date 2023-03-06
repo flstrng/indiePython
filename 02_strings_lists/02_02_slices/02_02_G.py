01d918988ea7491949738efabd5c9136138a21ac
flstrng <er8834567@gmail.com>
2023-03-06 17:35:59 +0300

    Solve 02_02_G

---
+++ 02_strings_lists/02_02_slices/02_02_G.py
@@ -0,0 +1,11 @@
+"""
+02_02_G
+Программа получает на вход строку. Ваша задача развернуть строку и вывести ее на экран.
+
+Sample Input:
+leetcode.com
+Sample Output:
+moc.edocteel
+"""
+phrase = input()
+print(phrase[::-1])
