a2cc19f8b1f7d9563a4fe7a8a50615c76d17761a
flstrng <er8834567@gmail.com>
2023-03-07 16:50:15 +0300

    Solve 02_03_02_B

---
+++ 02_strings_lists/02_03_strings_methods_2/02_03_02_B.py
@@ -0,0 +1,13 @@
+"""
+02_03_02_B
+Ниже перед вами представлен список list_strings, состоящий из строк.
+list_strings = ['Follow', 'the', 'Cops', 'Back', 'Home']
+При помощи метода .join и соединителя - получите строку из этих элементов и выведите ее на экран
+
+Sample Input:
+Sample Output:
+Follow-the-Cops-Back-Home
+"""
+
+list_strings = ['Follow', 'the', 'Cops', 'Back', 'Home']
+print('-'.join(list_strings))
